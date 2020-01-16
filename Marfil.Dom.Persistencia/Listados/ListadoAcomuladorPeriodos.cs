6b29ead07a480d8882a6e2c3501f49c792eb65c8
Willians Melendez <willian_melendez@hotmail.com>
2020-01-16 09:39:56 -0400

    Adecuaciones en el listado acomulador de periodos, falta la consulta del filtrado.

--- Marfil.Dom.Persistencia/Listados/ListadoAcomuladorPeriodos.cs
+++ Marfil.Dom.Persistencia/Listados/ListadoAcomuladorPeriodos.cs
@@ -2,9 +2,11 @@
 using Marfil.Dom.Persistencia.ServicesView.Servicios;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RAPeriodos = Marfil.Inf.ResourcesGlobalization.Textos.Entidades.ListadoAcomuladoPeriodos;
 
 namespace Marfil.Dom.Persistencia.Listados
 {
@@ -16,15 +18,40 @@ namespace Marfil.Dom.Persistencia.Listados
         #endregion
 
         #region Propiedades
+        [Display(ResourceType = typeof(RAPeriodos), Name = "FechaDesde")]
         public DateTime FechaDesde { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "FechaHasta")]
         public DateTime FechaHasta { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "SeccionDesde")]
+        public int SeccionDesde { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "SeccionHasta")]
+        public int SeccionHasta { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "Ejercicio")]
         public bool Ejercicio { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "Existencia")]
         public bool Existencia { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "Grupos")]
         public bool Grupos { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "CierreEjercicio")]
         public bool CierreEjercicio { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "IncluirAsientosSimulacion")]
         public bool IncluirAsientosSimulacion { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "ExcluirAsientosSimulacion")]
         public bool ExcluirAsientosSimulacion { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "IncluirAjusteExistenciaPeriodo")]
         public bool IncluirAjusteExistenciaPeriodo { get; set; }
+
+        [Display(ResourceType = typeof(RAPeriodos), Name = "IncluirProrrateoAmortizaciones")]
         public bool IncluirProrrateoAmortizaciones { get; set; }
         #endregion
         public ListadoAcomuladorPeriodos()
